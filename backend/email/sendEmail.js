99547cab67e5681811ef24ebf8d85078c51c7144
Raj maddheshiya <rajmaddheshiya-kumarmaddeshiyaraj@gmail.com>
2026-02-21 10:58:06 +0530

    fix: email timeout issue using port 587 and tls settings

--- backend/email/sendEmail.js
+++ backend/email/sendEmail.js
@@ -1,30 +1,33 @@
-import nodemailer from "nodemailer"; 
-export const sendOTPByEmail = async (email, otp) => { 
+import nodemailer from "nodemailer";
+export const sendOTPByEmail = async (email, otp) => {
     try {
-        
+
         const transporter = nodemailer.createTransport({
-            service: "gmail", 
+            host: "smtp.gmail.com",
+            port: 587,
+            secure: false, // 587 ke liye false hi rahega
             auth: {
-                user: process.env.EMAIL_AUTH, 
-                pass: process.env.EMAIL_PASS, 
+                user: process.env.EMAIL_AUTH,
+                pass: process.env.EMAIL_PASS,
             },
+            tls: {
+                rejectUnauthorized: false // Ye connection ko block hone se rokta hai
+            }
         });
 
-        
         const mailOptions = {
-            from:process.env.EMAIL_AUTH,
-            to: email, 
-            subject: "Account Verification OTP", 
-            text: `Aapka OTP hai: ${otp}`, 
-            html: `<h1 style="color:white;text-align:center;background-color:green;margin-top:9%;">OTP: ${otp}</h1>`, 
+            from: process.env.EMAIL_AUTH,
+            to: email,
+            subject: "Account Verification OTP",
+            text: `Aapka OTP hai: ${otp}`,
+            html: `<h1 style="color:white;text-align:center;background-color:green;margin-top:9%;">OTP: ${otp}</h1>`,
         };
 
-        await transporter.sendMail(mailOptions); 
-        console.log("Email sent!"); 
+        await transporter.sendMail(mailOptions);
+        console.log("Email sent!");
     } catch (error) {
-        console.log("Email error:", error); 
+        console.log("Email error:", error);
     }
 };
 
 
- 
