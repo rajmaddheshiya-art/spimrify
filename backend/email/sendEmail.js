08f91b560f66f30a5bcc77a0ec48018e8487c940
Raj maddheshiya <rajmaddheshiya-kumarmaddeshiyaraj@gmail.com>
2026-02-21 11:06:49 +0530

    final fix: forced ipv4 and increased timeouts for email

--- backend/email/sendEmail.js
+++ backend/email/sendEmail.js
@@ -2,19 +2,26 @@ import nodemailer from "nodemailer";
 export const sendOTPByEmail = async (email, otp) => {
     try {
 
-        const transporter = nodemailer.createTransport({
-            host: "smtp.gmail.com",
-            port: 587,
-            secure: false, // 587 ke liye false hi rahega
-            auth: {
-                user: process.env.EMAIL_AUTH,
-                pass: process.env.EMAIL_PASS,
-            },
-            tls: {
-                rejectUnauthorized: false // Ye connection ko block hone se rokta hai
-            }
-        });
-
+       const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, 
+    auth: {
+        user: process.env.EMAIL_AUTH,
+        pass: process.env.EMAIL_PASS,
+    },
+    // YE VALI SETTING ZAROORI HAI IPv6 ERROR KE LIYE
+    connectionTimeout: 10000, // 10 seconds
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
+    dnsTimeout: 10000,
+    // Forced IPv4
+    family: 4, 
+    tls: {
+        rejectUnauthorized: false,
+        minVersion: "TLSv1.2"
+    }
+});
         const mailOptions = {
             from: process.env.EMAIL_AUTH,
             to: email,
